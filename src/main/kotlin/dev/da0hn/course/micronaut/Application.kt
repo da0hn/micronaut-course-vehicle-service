83a17705695b59065e78b8bcbd60484b596cad93
Gabriel Honda <gabrielhondacba@gmail.com>
2024-02-01 17:20:56 -0400

    feat: commit inicial 🎉

---
+++ src/main/kotlin/dev/da0hn/course/micronaut/Application.kt
@@ -0,0 +1,8 @@
+package dev.da0hn.course.micronaut
+
+import io.micronaut.runtime.Micronaut.run
+
+fun main(args: Array<String>) {
+  run(*args)
+}
+
---
+++ src/test/kotlin/dev/da0hn/course/micronaut/VehicleServiceTest.kt
@@ -0,0 +1,20 @@
+package dev.da0hn.course.micronaut
+
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import jakarta.inject.Inject
+
+@MicronautTest
+class VehicleServiceTest {
+
+  @Inject
+  lateinit var application: EmbeddedApplication<*>
+
+  @Test
+  fun testItWorks() {
+    Assertions.assertTrue(application.isRunning)
+  }
+
+}
